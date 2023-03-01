65118d9116607480c0c1ff99e100458e8080b8ad
Max Lange <max@langes.us>
2023-03-01 00:35:41 -0500

    Remove unecessary react files + add base file structure

---
+++ src/Components/App/App.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <h1>Hello World!</h1>
+    )
+  }
+
+}
+
+export default App;
