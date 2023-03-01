9a6de1529d2cfecda71cb80f95e06f3aac4e8f9d
Max Lange <max@langes.us>
2023-03-01 01:06:15 -0500

    Add state, apiCall on componentDidMount, and route for EventGrid

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,19 +1,39 @@
 import React, {Component} from 'react';
-import Header from '../Header/Header';
 import { Route, Link, Switch } from 'react-router-dom';
+import EventGrid from '../EventGrid/EventGrid';
+import Header from '../Header/Header';
+import getEventInfo from '../../apiCalls';
 import './App.css';
 
 class App extends Component {
   constructor(){
     super()
     this.state = {
-
+      allData: [],
+      renderData: []
     }
+  }
+
+  componentDidMount() {
+    getEventInfo('').then(data => {
+      this.setState({allData: data, renderData: data})
+    })
   }
 
   render() {
     return (
-      <Header />
+      <>
+        <Header />
+        <Switch>
+          <Route exact path="/"
+            render={() => (
+              <EventGrid data={this.state.renderData} />
+            )}
+          />
+            
+          
+        </Switch>
+      </>
     )
   }
 
