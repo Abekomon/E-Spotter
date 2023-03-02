9a16ed5af09f5484e989c75fc89ead3a68526831
Max Lange <max@langes.us>
2023-03-01 19:27:35 -0500

    Add favorite button, that updates it's text on click

--- src/Components/EventInfo/EventInfo.js
+++ src/Components/EventInfo/EventInfo.js
@@ -1,12 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./EventInfo.css"
 
-export default function EventInfo({event}) {
+function updateText() {
+  document.querySelector('.info-fav-button').innerText = "Added to Favorites!"
+} 
+
+export default function EventInfo({addToFavorites, event}) {
   return (
     <div className="infoContainer">
-      <h2>{event.serie.full_name}</h2>
-      <h3>{event.videogame.name}</h3>
-      <p>{event.begin_at}</p>
+      <header className="infoHeader">
+        <Link className="info-home-link" to="/">Back to home</Link>
+        <button onClick={() => {
+            updateText()
+            addToFavorites(event.id)
+          }} 
+          className="info-fav-button">Add to Favorites</button>
+      </header>
+      <div className="infoBox">
+        <h2>{event.serie.full_name}</h2>
+        <h3>{event.videogame.name}</h3>
+        <p>{event.begin_at}</p>
+      </div>
     </div>
   )
 }
