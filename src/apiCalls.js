29297d8b31e64b8b2d356331eda58900ef3c1c72
Max Lange <max@langes.us>
2023-03-02 20:40:34 -0500

    Update apiCalls to be modular and accept a game param

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,5 +1,5 @@
-export default function getEventInfo() {
+export default function getEventInfo(game) {
   const options = {method: 'GET', headers: {accept: 'application/json'}};
-  return fetch('https://api.pandascore.co/tournaments/upcoming?token=gUY8h-9Zry0C1OcBMIZgHmjuRlKf_sR3i9jG_YIsaWKEEtYDIHo', options)
+  return fetch(`https://api.pandascore.co${game}/tournaments/upcoming?token=gUY8h-9Zry0C1OcBMIZgHmjuRlKf_sR3i9jG_YIsaWKEEtYDIHo`, options)
     .then(response => response.json())
 }
